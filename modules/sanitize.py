7f217c7d75d92d6f798f3f0652d3fe45ecb2357b
Raelon Masters <admin@mylocaltech.co>
2019-06-26 23:17:08 -0400

    Wrote the sanitize module and test

---
+++ modules/sanitize.py
@@ -0,0 +1,36 @@
+class Sanitize():
+    """
+        Data should be in the following format
+        DATE, TIME, OPEN, HIGH, LOW, CLOSE, VOLUME
+        Lets not trust our data
+    """
+    def __init__(self, data):
+        self.data = self.input(data)
+        return
+
+    def input(self, data):
+        try:
+            # Is input a file?
+            self.analyse(data)
+        except Exception as e:
+            # Nope it wasnt a file
+            raise e
+        return
+
+    def analyse(self, data):
+        import csv
+        import re
+        text_input = re.compile("^([A-Z])")
+        y = '[0-9]{4,4}'
+        m = '[01-12]{2,2}'
+        d = '[01-31]{2,2}'
+        with open(data, newline='') as csvfile:
+            input = csv.reader(csvfile, delimiter=',')
+            for row in input:
+                if text_input.match(row[0]):
+                    pass
+                else:
+                    # Work on input data here
+                    print(', '.join(row))
+            return
+        raise FileNotFoundError
---
+++ tests/test_sanitize.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from modules import sanitize
+
+class Test_Sanitize(unittest.TestCase):
+
+    def test_csvfile(self):
+        test_case = sanitize
+        csv_file = 'dumps/M5_hist_EURUSD.csv'
+        self.assertRaises(Exception, test_case.Sanitize(csv_file))
+
+
+if __name__ == '__main__':
+    unittest.main()
