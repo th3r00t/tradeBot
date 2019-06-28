e9c001fb24ad8737c06fea3d3581dcfab41551a4
Raelon Masters <admin@mylocaltech.co>
2019-06-28 01:34:23 -0400

    Finished seperating dateTime, and arranging OHLCV

--- modules/sanitize.py
+++ modules/sanitize.py
@@ -11,27 +11,41 @@ class Sanitize():
     def input(self, data):
         try:
             # Is input a file?
-            self.analyse(data)
+            self.analyze(data)
         except Exception as e:
             # Nope it wasnt a file
             raise e
         return
-
-    def analyse(self, data):
+    
+    def findDateTime(self, row):
+        try:
+            date = row[0].split('.')
+            date.append(row[1])
+            return date
+        except:
+            raise KeyError
+        return
+    
+    def analyze(self, data):
         import csv
         import re
-        import numpy as np
         text_input = re.compile("^([A-Z])")
-        y = '[0-9]{4,4}'
-        m = '[01-12]{2,2}'
-        d = '[01-31]{2,2}'
+        file = open(data, 'r')
+        file = csv.reader(file, delimiter=',')
+        ncol=len(next(file)) # Read first line and count columns
         with open(data, newline='') as csvfile:
             input = csv.reader(csvfile, delimiter=',')
             for row in input:
+                # Check for header
                 if text_input.match(row[0]):
                     pass
                 else:
                     # Work on input data here
-                    row,columns=row.shape()
+                    _dateTime = self.findDateTime(row)
+                    _open = row[2]
+                    _high = row[3]
+                    _low = row[3]
+                    _close = row[4]
+                    _volume = row[5]
             return
         raise FileNotFoundError
---
+++ modules/storage.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+
+class Database():
+
+    def __init__(self):
+        self.connection = sqlite3.connect()
